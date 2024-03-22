0a9c50389ca0c3a56cd463c8f13aa211d06be61a
geedtd <grcazares@gmail.com>
2024-03-21 21:13:46 -0700

    back to sphere

--- src/script.js
+++ src/script.js
@@ -118,30 +118,30 @@ gui.add(material, 'ior', 0, 10, 0.001)
 gui.add(material, 'thickness', 0, 10, 0.001)
 // gui.addColor(material, 'color')
 
-// Geometry
-// let geometry = new THREE.IcosahedronGeometry(2.5, 50)
-// geometry = mergeVertices(geometry)
-// geometry.computeTangents()
-// console.log(geometry.attributes)
+//Geometry
+let geometry = new THREE.IcosahedronGeometry(2.5, 50)
+geometry = mergeVertices(geometry)
+geometry.computeTangents()
+console.log(geometry.attributes)
 
-// // Mesh
-// const wobble = new THREE.Mesh(geometry, material)
-// wobble.customDepthMaterial = depthMaterial
-// wobble.receiveShadow = true
-// wobble.castShadow = true
-// scene.add(wobble)
+// Mesh
+const wobble = new THREE.Mesh(geometry, material)
+wobble.customDepthMaterial = depthMaterial
+wobble.receiveShadow = true
+wobble.castShadow = true
+scene.add(wobble)
 
 //Model
 
-gltfLoader.load('./suzanne.glb', (gltf) => {
-    const wobble = gltf.scene.children[0]
-    wobble.receiveShadow = true
-    wobble.castShadow = true
-    wobble.material = material
-    wobble.customDepthMaterial = depthMaterial
+// gltfLoader.load('./suzanne.glb', (gltf) => {
+//     const wobble = gltf.scene.children[0]
+//     wobble.receiveShadow = true
+//     wobble.castShadow = true
+//     wobble.material = material
+//     wobble.customDepthMaterial = depthMaterial
 
-    scene.add(wobble)
-})
+//     scene.add(wobble)
+// })
 
 /**
  * Plane
